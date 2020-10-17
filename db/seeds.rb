b55416912caf4e1d4b6ec45f2232f7ba431d6a3c
Roberto Caffaro <caffaro.filho@gmail.com>
2020-10-17 17:38:05 -0300

    Halfway through application

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: 'lemon')
-Ingredient.create(name: 'ice')
-Ingredient.create(name: 'mint leaves')
-Ingredient.create(name: 'vodka')
-Ingredient.create(name: 'rhum')
-Ingredient.create(name: 'tequila')
-Ingredient.create(name: 'orange juice')
-Ingredient.create(name: 'tomato juice')
+# Ingredient.create(name: 'lemon')
+# Ingredient.create(name: 'ice')
+# Ingredient.create(name: 'mint leaves')
+# Ingredient.create(name: 'vodka')
+# Ingredient.create(name: 'rhum')
+# Ingredient.create(name: 'tequila')
+# Ingredient.create(name: 'orange juice')
+# Ingredient.create(name: 'tomato juice')
+
+require 'json'
+require 'open-uri'
+
+Ingredient.destroy_all
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+drinks = JSON.parse(open(url).read)
+puts 'retrieving ingredients list...'
+drinks["drinks"].each do |i|
+  Ingredient.create(name: i["strIngredient1"])
+  puts "#{i["strIngredient1"]} added"
+end
